3f00168663445a83db48ff04edca76b9fdb43c9e
tunahan top <tunahan@tunahan-MacBook-Pro.local>
2025-08-26 14:43:39 +0300

    test kerem

--- test.sh
+++ test.sh
@@ -4,3 +4,24 @@ while true; # syntax err here
     sleep 1
     echo lol
 done
+
+if [ $1 -eq 0 # syntax err here
+    echo "Sıfır"
+fi
+
+if [ $1 -gt 0 # syntax err here
+    echo "Pozitif"
+fi
+
+for i in 1 2 3 # syntax err here
+    echo $i
+done
+
+while [ $i -lt 10 # syntax err here
+    echo $i
+    i=$((i+1))
+done
+
+if [ $1 -eq 0 ]; then
+    echo "Sıfır"
+# syntax err here
